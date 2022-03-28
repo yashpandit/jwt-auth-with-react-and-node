f9ccaae0b87432a8e6442301e62286b4ab4c45a9
Yash Pandit <hireyashpandit@gmail.com>
2022-03-28 11:57:09 +0530

    show posts as authenticated request

--- client/src/components/Success/index.jsx
+++ client/src/components/Success/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { initialAuthState } from "../../AuthProvider";
 import useAuthContext from "../../AuthProvider/hooks/useAuthContext";
 import useRequest from "../../hooks/useRequest";
@@ -9,6 +10,19 @@ const Success = () => {
     setState
   } = useAuthContext();
   const request = useRequest();
+  const [showPosts, setShowPosts] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  const getUserPosts = async () => {
+    const response = await request('posts', { authRequired: true });
+    setPosts(response.data);
+  };
+
+  useEffect(() => {
+    if (showPosts) {
+      getUserPosts();
+    }
+  }, [showPosts]);
 
   const handleLogout = async () => {
     try {
@@ -25,10 +39,25 @@ const Success = () => {
     }
   };
 
+  const handleViewPostsChange = () => setShowPosts(prev => !prev);
+
   return (
     <div className="flex flex-col gap-3 items-center justify-center">
       <h1>hey {username}, you are now logged in!</h1>
-      <Button theme="primary" onClick={handleLogout}>Logout</Button>
+      <div className="flex items-center gap-2">
+        <Button theme="primary" onClick={handleViewPostsChange}>{showPosts ? 'Hide' : 'View'} posts</Button>
+        <Button onClick={handleLogout}>Logout</Button>
+      </div>
+      {showPosts && posts.length > 0 && (
+        <div>
+          <h2 className="font-bold underline">Posts</h2>
+          <ul>
+            {posts.map(({ name, title }) => (
+              <li key={name}>Title: {title}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
--- client/src/hooks/useRequest.js
+++ client/src/hooks/useRequest.js
@@ -1,16 +1,19 @@
 import axios from 'axios';
+import useAuthContext from '../AuthProvider/hooks/useAuthContext';
 
 const useRequest = () => {
+  const { state: { accessToken } } = useAuthContext();
+
   const request = async (url, options) => {
-    const { method, body } = options;
+    const { method, body, authRequired } = options ?? {};
     const apiUrl = `http://localhost:4000/${url}`;
     const response = await axios({
       url: apiUrl,
       method: method || 'GET',
       ...(body ? { data: body } : {}),
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        ...(authRequired ? { 'Authorization': `Bearer ${accessToken}` } : {}),
       },
     });
 
